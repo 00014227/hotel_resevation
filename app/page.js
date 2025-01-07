95b499159fa6e2f1d7d2fca2bf386472c4021274
Ahmad <tulkinxojaevaxad@gmail.com>
2025-01-07 19:56:51 +0500

    date range picker

--- app/page.js
+++ app/page.js
@@ -1,13 +1,15 @@
+"use client"
 import Gutter from "@/components/Gutter";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import SearchForm from "@/components/SearchForm";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Gutter>
-
+        <SearchForm/>
 
       </Gutter>
     </>
